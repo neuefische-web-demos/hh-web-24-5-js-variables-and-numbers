30e70db964a88706e5c896eaa062f9d37f9ee40f
Niklas Röttger <niklas.roettger@outlook.de>
2024-06-05 10:18:17 +0200

    add session code

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
 console.clear();
+
+// number
+const myNumber = 42;
+// console.log(myNumber);
+
+let myMutableNumber = 42;
+// console.log(myMutableNumber);
+myMutableNumber = myMutableNumber + 1;
+// console.log(myMutableNumber);
+
+// boolean
+const isHungry = true;
+
+// string
+const name = "Niklas";
+
+// null
+const isNull = null;
+
+// undefined
+const isUndefined = undefined;
+
+// const oneTimeFee = "5";
+// const monthlyFee = 12;
+// const amountToBeCharged = oneTimeFee + monthlyFee;
+// console.log(amountToBeCharged);
+
+// Mathematical operators
+
+// +
+const oneTimeFee = 5;
+const monthlyFee = 12;
+const amountToBeCharged = oneTimeFee + monthlyFee;
+console.log(amountToBeCharged);
+
+// -
+const mortgageSum = 20000;
+const alreadyPayed = 500;
+const amountToBePayed = mortgageSum - alreadyPayed;
+console.log(amountToBePayed);
+
+// *
+const length = 15;
+const width = 4;
+const area = length * width;
+console.log(area);
+
+// /
+const students = 16;
+const groups = 4;
+const studentsPerGroup = students / groups;
+console.log(studentsPerGroup);
+
+// **
+console.log(2 ** 10);
+
+// % modulo
+console.log(8 % 3);
+
+// Assignment Operators
+
+// +=
+let balance = 20;
+// balance = balance + 5;
+balance += 5;
+console.log(balance);
+
+// -/
+let newBalance = 20;
+// newBalance = newBalance - 5;
+newBalance -= 5;
+console.log(newBalance);
+
+// *=
+let price = 8;
+// price = price * 2;
+price *= 2;
+console.log(price);
+
+// /=
+let newPrice = 8;
+// newPrice = newPrice / 4;
+newPrice /= 4;
+console.log(newPrice);
+
+// ++
+let veryNewPrice = 8;
+veryNewPrice++;
+console.log(veryNewPrice);
+
+// --
+veryNewPrice--;
+console.log(veryNewPrice);
